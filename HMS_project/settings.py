25075e6bd14a77ada03a34dde2afead5f26af2b6
Hana00661 <eng.hana95@gmail.com>
2024-10-18 07:53:32 +0400

    modify the static

--- HMS_project/settings.py
+++ HMS_project/settings.py
@@ -30,7 +30,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-oowm4u9r5$=49nzzgb#*9miy*=4lvp4t7&3fx7+f^pt^ogs)ka'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 CSRF_TRUSTED_ORIGINS = ['https://vclinic.kashtapp.com']
