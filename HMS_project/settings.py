e5f253220081721a0d3105f2d966b534016aa7e6
Hana00661 <eng.hana95@gmail.com>
2024-10-21 23:28:54 +0400

    edit the django.yml

--- HMS_project/settings.py
+++ HMS_project/settings.py
@@ -61,9 +61,6 @@ INSTALLED_APPS = [
     'django.contrib.sites',  # Required by allauth
     'allauth',
     'allauth.account',
-    'allauth.socialaccount',  # Enable for social logins
-    
-    'allauth.socialaccount.providers.google',  # Example provider
 
 ]
 
